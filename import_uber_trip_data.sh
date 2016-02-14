0f0860188177f6bf6d68dbe98f180f04d9b376de
Todd Schneider <todd.w.schneider@gmail.com>
2016-02-14 10:11:21 -0500

    set datestyle for uber 2014 data

--- import_uber_trip_data.sh
+++ import_uber_trip_data.sh
@@ -3,7 +3,7 @@
 # load 2014 Uber data into unified `trips` table
 for filename in data/uber*14.csv; do
   echo "`date`: beginning load for $filename"
-  cat $filename | psql nyc-taxi-data -c "COPY uber_trips_staging (pickup_datetime, pickup_latitude, pickup_longitude, base_code) FROM stdin CSV HEADER;"
+  cat $filename | psql nyc-taxi-data -c "SET datestyle = 'ISO, MDY'; COPY uber_trips_staging (pickup_datetime, pickup_latitude, pickup_longitude, base_code) FROM stdin CSV HEADER;"
   echo "`date`: finished raw load for $filename"
   psql nyc-taxi-data -f populate_uber_trips.sql
   echo "`date`: loaded trips for $filename"
