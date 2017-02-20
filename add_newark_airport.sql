8f9ed63cb115e8a5d937ebfb3f55b811197a7e35
Todd Schneider <todd.w.schneider@gmail.com>
2017-02-20 08:33:07 -0500

    use newark airport geom from taxi_zones shapefile to create fake census tract

--- add_newark_airport.sql
+++ add_newark_airport.sql
@@ -4,9 +4,4 @@ SELECT
   'New Jersey',
   'Newark Airport',
   'NJ01',
-  ST_GeomFromText('MULTIPOLYGON(((-74.1795837 40.6697509,
-                                  -74.1972654 40.6774568,
-                                  -74.1922779 40.6900883,
-                                  -74.1804249 40.7077294,
-                                  -74.1518226 40.7072984,
-                                  -74.1795837 40.6697509)))', 4326);
+  (SELECT geom FROM taxi_zones WHERE zone = 'Newark Airport');
