b92ead61e19e95fc1401d4666cf8f2ac0e2e0ccc
Todd Schneider <todd.w.schneider@gmail.com>
2017-03-01 20:23:54 -0500

    upcase and trim fhv base numbers

--- import_fhv_trip_data.sh
+++ import_fhv_trip_data.sh
@@ -6,4 +6,7 @@ for filename in data/fhv*.csv; do
   echo "`date`: loaded trips for $filename"
 done;
 
+psql nyc-taxi-data -c "UPDATE fhv_trips SET dispatching_base_num = trim(upper(dispatching_base_num)) WHERE dispatching_base_num != trim(upper(dispatching_base_num));"
+psql nyc-taxi-data -c "VACUUM FULL ANALYZE fhv_trips;"
+
 psql nyc-taxi-data -c "CREATE INDEX index_fhv_on_location ON fhv_trips (location_id);"
