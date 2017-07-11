e8d183b9f2bdf4df4c6c114c85e16cd9b3493d9c
Todd Schneider <todd.w.schneider@gmail.com>
2017-07-10 20:21:23 -0400

    add data from first half of 2017

--- import_trip_data.sh
+++ import_trip_data.sh
@@ -10,11 +10,15 @@ green_schema_2015_h2_2016_h1="(vendor_id,lpep_pickup_datetime,lpep_dropoff_datet
 
 green_schema_2016_h2="(vendor_id,lpep_pickup_datetime,lpep_dropoff_datetime,store_and_fwd_flag,rate_code_id,pickup_location_id,dropoff_location_id,passenger_count,trip_distance,fare_amount,extra,mta_tax,tip_amount,tolls_amount,ehail_fee,improvement_surcharge,total_amount,payment_type,trip_type,junk1,junk2)"
 
+green_schema_2017_h1="(vendor_id,lpep_pickup_datetime,lpep_dropoff_datetime,store_and_fwd_flag,rate_code_id,pickup_location_id,dropoff_location_id,passenger_count,trip_distance,fare_amount,extra,mta_tax,tip_amount,tolls_amount,ehail_fee,improvement_surcharge,total_amount,payment_type,trip_type)"
+
 yellow_schema_pre_2015="(vendor_id,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,pickup_longitude,pickup_latitude,rate_code_id,store_and_fwd_flag,dropoff_longitude,dropoff_latitude,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,total_amount)"
 
 yellow_schema_2015_2016_h1="(vendor_id,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,pickup_longitude,pickup_latitude,rate_code_id,store_and_fwd_flag,dropoff_longitude,dropoff_latitude,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount)"
 
 yellow_schema_2016_h2="(vendor_id,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,rate_code_id,store_and_fwd_flag,pickup_location_id,dropoff_location_id,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount,junk1,junk2)"
+
+yellow_schema_2017_h1="(vendor_id,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,rate_code_id,store_and_fwd_flag,pickup_location_id,dropoff_location_id,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount)"
 
 # if 2010-02 and 2010-03 yellow files give errors about extra columns, remove offending rows:
 # sed -E '/(.*,){18,}/d' data/yellow_tripdata_2010-02.csv > data/yellow_tripdata_2010-02.csv
@@ -31,8 +35,10 @@ for filename in data/green*.csv; do
     schema=$green_schema_2015_h1
   elif [ $year -eq 2015 ] || ([ $year -eq 2016 ] && [ $month -lt 7 ]); then
     schema=$green_schema_2015_h2_2016_h1
-  else
+  elif [ $year -eq 2016 ] && [ $month -gt 6 ]; then
     schema=$green_schema_2016_h2
+  else
+    schema=$green_schema_2017_h1
   fi
 
   echo "`date`: beginning load for ${filename}"
@@ -51,8 +57,10 @@ for filename in data/yellow*.csv; do
     schema=$yellow_schema_pre_2015
   elif [ $year -eq 2015 ] || ([ $year -eq 2016 ] && [ $month -lt 7 ]); then
     schema=$yellow_schema_2015_2016_h1
-  else
+  elif [ $year -eq 2016 ] && [ $month -gt 6 ]; then
     schema=$yellow_schema_2016_h2
+  else
+    schema=$yellow_schema_2017_h1
   fi
 
   echo "`date`: beginning load for ${filename}"
